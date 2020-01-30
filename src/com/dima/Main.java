6f53fa30a48174ecf54e27c4256356d6a92090a7
Dmitri <dmitri.bogdanovits@trytrue.com>
2020-01-30 12:50:56 +0200

    test

--- src/com/dima/Main.java
+++ src/com/dima/Main.java
@@ -3,9 +3,11 @@ package com.dima;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("Hello World");
+        new Main().entryPoint(args);
+    }
 
+    public void entryPoint(String[] args) {
+        System.out.println("Hello World");
 
         int i = 0;
         long l = 0;
@@ -23,32 +25,42 @@ public class Main {
 
 
         for (int j = 0; j < 5; j += 2) {
-            System.out.println(">>> j: " + j);
+            // System.out.println(">>> j: " + j);
         }
         System.out.println("=========");
         int y = 0;
 
-        do {
-            if (y % 2 == 0) {
-                System.out.println(">>> " + y);
-            }
-            y++;
-        } while (y < 50);
+//        do {
+//            if (y % 2 == 0) {
+//                System.out.println(">>> " + y);
+//            }
+//            y++;
+//        } while (y < 50);
 
 
-        switch (y) {
-            case 1:
-                System.out.println(">>> 1");
-                break;
+//        switch (y) {
+//            case 1:
+//                printString2("1");
+//                break;
+//
+//            case 2:
+//                printString2("2");
+//                break;
+//
+//            default:
+//
+//        }
+        int sum = sum(2, 6);
+        System.out.println(">>> sum : " + sum);
 
-            case 2:
-                System.out.println(">>> 2");
-                break;
 
-            default:
+    }
 
-        }
+    public void printString2(String s) {
+        System.out.println(">>> " + s);
+    }
 
-
+    int sum(int a, int b) {
+        return a + b;
     }
 }
